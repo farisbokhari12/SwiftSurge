55651d2d41250ed3f1164729f0b0134e875154aa
haiderbok2 <haiderbokha@gmail.com>
2023-07-09 01:25:06 -0700

    sqush this: working example of geolocation cords

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,7 +1,7 @@
 import { IonButton } from '@ionic/react';
 import './ExploreContainer.css';
 import { useEffect, useState } from 'react';
-import { Geolocation } from "@awesome-cordova-plugins/geolocation";
+import { Coordinates, Geolocation } from "@awesome-cordova-plugins/geolocation";
 
 interface ContainerProps {
   name: string;
@@ -9,10 +9,10 @@ interface ContainerProps {
 
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-
+const [coords, setCoords] = useState<Coordinates>();
   const geoloactionWrapper  = async () => {
     const geoloaction =  await Geolocation.getCurrentPosition();
-    console.log(geoloaction.coords);
+    setCoords(geoloaction.coords);
   }
 
   return (
@@ -20,6 +20,7 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
       <strong>{name}</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       <IonButton onClick={geoloactionWrapper}>gila</IonButton>
+      <IonButton onClick={geoloactionWrapper}>{ coords?.latitude }</IonButton>
     </div>
   );
 };
